5d68df0bbaa3c336184d2527a2ad6296752d2b9e
左程云 <zuochengyun@zuochengyundeMacBook-Pro.local>
2020-08-15 05:59:21 +0800

    modify code

--- src/toplikedquestions/Problem_0621_TaskScheduler.java
+++ src/toplikedquestions/Problem_0621_TaskScheduler.java
@@ -2,14 +2,14 @@ package toplikedquestions;
 
 public class Problem_0621_TaskScheduler {
 
-    public static int leastInterval(char[] tasks, int n) {
-        int[] count = new int[256];
+	public static int leastInterval(char[] tasks, int n) {
+		int[] count = new int[256];
 		int maxCount = 0;
 		for (char task : tasks) {
 			count[task]++;
 			maxCount = Math.max(maxCount, count[task]);
 		}
-        int maxKinds = 0;
+		int maxKinds = 0;
 		for (int task = 0; task < 256; task++) {
 			if (count[task] == maxCount) {
 				maxKinds++;
