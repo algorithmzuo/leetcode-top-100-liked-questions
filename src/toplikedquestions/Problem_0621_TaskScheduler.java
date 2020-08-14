83644d0654c6976e18625f05312f8109ebb9f0fa
左程云 <zuochengyun@zuochengyundeMacBook-Pro.local>
2020-08-15 05:39:12 +0800

    modify code

--- src/toplikedquestions/Problem_0621_TaskScheduler.java
+++ src/toplikedquestions/Problem_0621_TaskScheduler.java
@@ -16,9 +16,9 @@ public class Problem_0621_TaskScheduler {
 			}
 		}
 		int N = tasks.length;
-		int onePartLen = Math.max(0, limit + 1 - maxKinds);
-		int fillParts = maxCount - 1;
-		int enoughSpaces = fillParts * onePartLen;
+		int spacePartLen = Math.max(0, limit + 1 - maxKinds);
+		int spaceParts = maxCount - 1;
+		int enoughSpaces = spaceParts * spacePartLen;
 		int usedSpaces = N - maxCount * maxKinds;
 		int restSpaces = Math.max(0, enoughSpaces - usedSpaces);
 		return N + restSpaces;
