55a4957832c9fefe37c3e9c05c1b867c66bd37d1
algorithmzuo <algorithmzuo@163.com>
2021-04-13 20:12:15 +0800

    modify code

--- src/toplikedquestions/Problem_0494_TargetSum.java
+++ src/toplikedquestions/Problem_0494_TargetSum.java
@@ -1,109 +1,87 @@
 package toplikedquestions;
 
+import java.util.HashMap;
+
 public class Problem_0494_TargetSum {
 
-	public static int findWays(int[] arr, int s) {
-		if (arr == null || arr.length == 0) {
-			return s == 0 ? 1 : 0;
-		}
-		return ways(arr, 0, s);
+	public static int findTargetSumWays1(int[] arr, int s) {
+		return process1(arr, 0, s);
 	}
 
-	// dp[index][rest]
-	public static int ways(int[] arr, int index, int rest) {
+	public static int process1(int[] arr, int index, int rest) {
 		if (index == arr.length) {
 			return rest == 0 ? 1 : 0;
 		}
-		int p1 = ways(arr, index + 1, rest + arr[index]);
-		int p2 = ways(arr, index + 1, rest - arr[index]);
-		return p1 + p2;
+		return process1(arr, index + 1, rest - arr[index]) + process1(arr, index + 1, rest + arr[index]);
 	}
 
-	public static int dp(int[] arr, int s) {
-		if (arr == null || arr.length == 0) {
-			return s == 0 ? 1 : 0;
-		}
-		int sum = 0;
-		for (int num : arr) {
-			sum += num;
-		}
-		if (sum < s) {
-			return 0;
-		}
-		int N = arr.length;
-		int[][] dp = new int[N + 1][sum * 2 + 1];
-		dp[N][0 + sum] = 1;
-		for (int index = N - 1; index >= 0; index--) {
-			for (int rest = -sum; rest <= sum; rest++) {
-				dp[index][sum + rest] = 0;
-				if (sum + rest + arr[index] <= 2 * sum) {
-					dp[index][sum + rest] += dp[index + 1][sum + rest + arr[index]];
-				}
-				if (sum + rest - arr[index] >= 0) {
-					dp[index][sum + rest] += dp[index + 1][sum + rest - arr[index]];
-				}
-			}
-		}
-		return dp[0][sum + s];
+	public static int findTargetSumWays2(int[] arr, int s) {
+		return process2(arr, 0, s, new HashMap<>());
 	}
 
-	// 题目已知arr中都是非负数
-	// arr中每个数都能在之前添加+或者-，想最后的结果是num，返回方法数
-	public static int findTargetSumWays1(int[] arr, int num) {
-		return process(arr, num, 0, 0);
-	}
-
-	// arr[0..index-1]的范围已经添加过符号了，具体是什么不用操心，反正形成了pre的结果
-	// 现在请在arr[index...]上添加符号，请问在之前结果为pre的情况下，
-	// 最终形成num的方法数是多少？返回
-	public static int process(int[] arr, int num, int index, int pre) {
+	public static int process2(int[] arr, int index, int rest, HashMap<Integer, HashMap<Integer, Integer>> dp) {
+		if (dp.containsKey(index) && dp.get(index).containsKey(rest)) {
+			return dp.get(index).get(rest);
+		}
+		int ans = 0;
 		if (index == arr.length) {
-			return pre == num ? 1 : 0;
+			ans = rest == 0 ? 1 : 0;
+		} else {
+			ans = process2(arr, index + 1, rest - arr[index], dp) + process2(arr, index + 1, rest + arr[index], dp);
 		}
-		// 添加+号，形成的方法数
-		int ways = process(arr, num, index + 1, pre + arr[index]);
-		// 再累加上，添加-号，形成的方法数
-		ways += process(arr, num, index + 1, pre - arr[index]);
-		return ways;
+		if (!dp.containsKey(index)) {
+			dp.put(index, new HashMap<>());
+		}
+		dp.get(index).put(rest, ans);
+		return ans;
 	}
 
-	public static int findTargetSumWays2(int[] arr, int num) {
-		int N = arr.length;
-		// index变化范围是0..N
-		// 如果arr所有数字的累加和是sum，那么pre的范围是：-sum ~ +sum
-		// 所以改成记忆化搜索可能更好，不过这里无所谓了，改成严格表结构吧
+	// 优化点一 :
+	// 你可以认为arr中都是非负数
+	// 因为即便是arr中有负数，比如[3,-4,2]
+	// 因为你能在每个数前面用+或者-号
+	// 所以[3,-4,2]其实和[3,4,2]达成一样的效果
+	// 那么我们就全把arr变成非负数，不会影响结果的
+	// 优化点二 :
+	// 如果arr都是非负数，并且所有数的累加和是sum
+	// 那么如果target<sum，很明显没有任何方法可以达到target，可以直接返回0
+	// 优化点三 :
+	// arr内部的数组，不管怎么+和-，最终的结果都一定不会改变奇偶性
+	// 所以，如果所有数的累加和是sum，
+	// 并且与target的奇偶性不一样，没有任何方法可以达到target，可以直接返回0
+	// 优化点四 :
+	// 比如说给定一个数组, arr = [1, 2, 3, 4, 5] 并且 target = 3
+	// 其中一个方案是 : +1 -2 +3 -4 +5 = 3
+	// 该方案中取了正的集合为P = {1，3，5}
+	// 该方案中取了负的集合为N = {2，4}
+	// 所以任何一种方案，都一定有 sum(P) - sum(N) = target
+	// 现在我们来处理一下这个等式，把左右两边都加上sum(P) + sum(N)，那么就会变成如下：
+	// sum(P) - sum(N) + sum(P) + sum(N) = target + sum(P) + sum(N)
+	// 2 * sum(P) = target + 数组所有数的累加和
+	// sum(P) = (target + 数组所有数的累加和) / 2
+	// 也就是说，任何一个集合，只要累加和是(target + 数组所有数的累加和) / 2
+	// 那么就一定对应一种target的方式
+	// 也就是说，比如非负数组arr，target = 7, 而所有数累加和是11
+	// 求有多少方法组成7，其实就是求有多少种达到累加和(7+11)/2=9的方法
+	// 优化点五 :
+	// 二维动态规划的空间压缩技巧
+	public static int findTargetSumWays3(int[] arr, int s) {
 		int sum = 0;
-		for (int cur : arr) {
-			sum += cur;
+		for (int n : arr) {
+			sum += n;
 		}
-		if (num > sum) {
-			return 0;
-		}
-		int M = 2 * sum + 1;
-		int[][] dp = new int[N + 1][M];
-		// 举个例子，如果arr，累加和是10，那么pre有可能的范围是-10到+10
-		// 所以我们需要准备21列，去对应pre的变化范围，也就是dp[N+1][2 * sum + 1]
-		// 我们规定如何表示pre：用dp[...][sum+pre]来表示，当然也可以有其他的表示。
-		// 如何表示pre=0的时候？dp[...][10+0]->用dp[...][10]来表示
-		// 如何表示pre=1的时候？dp[...][10+1]->用dp[...][11]来表示
-		// 如何表示pre=5的时候？dp[...][10+5]->用dp[...][15]来表示
-		// 如何表示pre=10的时候？dp[...][10+10]->用dp[...][20]来表示
-		// 如何表示pre=-1的时候？dp[...][10+(-1)]->用dp[...][9]来表示
-		// 如何表示pre=-5的时候？dp[...][10+(-5)]->用dp[...][5]来表示
-		// 如何表示pre=-10的时候？dp[...][10+(-10)]->用dp[...][0]来表示
-		// 所以pre等于多少，都可以表示下
-		dp[N][sum + num] = 1;
-		for (int index = N - 1; index >= 0; index--) {
-			for (int pre = -sum; pre <= sum; pre++) {
-				if (sum + pre + arr[index] < M) {
-					dp[index][sum + pre] = dp[index + 1][sum + pre + arr[index]];
-				}
-				if (sum + pre - arr[index] >= 0) {
-					dp[index][sum + pre] += dp[index + 1][sum + pre - arr[index]];
-				}
+		return sum < s || ((s & 1) ^ (sum & 1)) != 0 ? 0 : subset(arr, (s + sum) >> 1);
+	}
+
+	public static int subset(int[] nums, int s) {
+		int[] dp = new int[s + 1];
+		dp[0] = 1;
+		for (int n : nums) {
+			for (int i = s; i >= n; i--) {
+				dp[i] += dp[i - n];
 			}
 		}
-		return dp[0][sum + 0];
+		return dp[s];
 	}
 
 }
